4981ee2d49920551339a47203b8ec74add85eca6
somyungseop <69204753+somyungseop@users.noreply.github.com>
2020-08-05 17:25:27 +0900

    data_type(number)
    숫자 자료형의 표현

--- day_1.py
+++
@@ -1,3 +0,0 @@
-print("hello world")
-print(5)
-print(3.14)
---
+++ practice_16days/day_1.py
@@ -0,0 +1,19 @@
+#Data type_number
+
+a = [3,3.14,"4.24E10","0o177","0x177"]
+for number in a:
+    if number in [3]:
+        print("%s 은/는 정수형입니다." %number)
+    elif number in [3.14]:
+        print("%s 은/는 실수형입니다." %number)
+    elif number in ["4.24E10"]:
+        print("%s 은/는 컴퓨터형 지수표현방식입니다.(e와E 무엇을 사용하여도 됩니다.)" %number)
+    elif number in ["0o177"]:
+        print("%s 은/는 8진수입니다.(알파벳 o의 대,소문자는 무관합니다.)" %number)
+    else:
+        print("%s 은/는 16진수입니다.(알파벳 x의 대,소문자는 무관합니다.)" %number)        
+
+print(=*50)   #숫자형 종료
+
+
+                
